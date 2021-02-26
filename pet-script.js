aa7b362d3808d543c2e168c2a06c594dabe3480c
Cashman1396 <Chrisashman96@gmail.com>
2021-02-26 12:45:27 -0500

    added JS button for downloading

--- pet-script.js
+++ pet-script.js
@@ -1,3 +1,10 @@
 //creates Array for Users and loads in images from the back end
 const imgGallery = new Array()
 
+
+//creates elements for downloading the images in the gallery
+const downloadBtn = doucment.createElement("div")
+downloadBtn.classList.add("downloadBtn")
+
+const downloadImg = document.createElement("button")
+downloadImg.innerHTML = "Download these images"
