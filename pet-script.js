b04b1140065e0ee305fc2c8fde418af8736ffd95
Cashman1396 <Chrisashman96@gmail.com>
2021-02-26 16:14:26 -0500

    added fetch request using axios first learning this

--- pet-script.js
+++ pet-script.js
@@ -3,14 +3,14 @@ const imgGallery = new Array()
 
 
 //creates elements for downloading the images in the gallery
-const download = doucment.createElement("div")
-download.classList.add("download")
+const header = document.createElement("div")
+header.classList.add("download")
 
-const downloadImg = document.createElement("button")
-downloadImg.innerHTML = "Download these images"
-downloadImg.classList.add("select-all-button")
+const downloadImgs = document.createElement("button")
+downloadImgs.innerHTML = "Download images"
+downloadImgs.classList.add("select-all-button")
 
-download.appendChild(downloadImg)
+header.appendChild(downloadImgs)
 
 
 //creation of images and retreiving from server
@@ -18,7 +18,7 @@ const getImage = (data) => {
     const rootDiv = document.createElement("div")
     rootDiv.classList.add("img-container")
 
-    const img = doucment.createElement("img")
+    const img = doucument.createElement("img")
     img.classList.add("img-pets")
     img.crossOrigin = "Anonymous"
     img.src = data.url
@@ -33,7 +33,7 @@ const getImage = (data) => {
 
     const petDate = doucment.createElement("p")
     const date = data.created.split(" ")
-    petDate.innerHTML = `This pet joined on ${date[0]}, ${date[1]}, ${date[2]}, ${date[date[date.length - 1]]}}`
+    petDate.innerHTML = `This pet joined on ${date[0]}, ${date[1]}, ${date[2]}, ${date[date[date.length - 1]]}`
     petDate.classList.add("pet-date")
 
     //description of the pet
@@ -55,16 +55,16 @@ const getImage = (data) => {
         }
         else {
             imgGallery.push(data)
-            img.classList.add('"selected-img')            
+            img.classList.add("selected-img")            
         }
 
 
         if (imgGallery.length === 0) {
-            document.querySelector("download-page").removeChild(download)
+            document.querySelector("download").removeChild(download)
         } 
         else 
         {
-            document.querySelector("download-page").appendChild(download) 
+            document.querySelector("download").appendChild(download) 
         }
     })
 
@@ -72,8 +72,44 @@ const getImage = (data) => {
     text.appendChild(petDate)
     text.appendChild(description)
 
-
-
-
-
 }
+
+
+//calls for download uses axios
+function callDownload(str) {
+    axios({
+        url: str.url,
+        method: "GET",
+        responseType: "blob"
+        })
+        .then((response) => {
+            const url = window.webkitURL.createObjectURL(new Blob([response.data]))
+            const link = document.createElement("a")
+            link.href = url
+            link.setAttribute("download", `${str.title.replace(/ /g, "_")}.jpeg`)
+            document.body.appendChild(link)
+            link.click()
+        })
+}
+
+//eventListener for when you download all images
+downloadImgs.addEventListener("click", () => {
+    imgGallery,forEach(e => {
+        callDownload(e)
+    })
+})
+
+const getPets = async () => {
+    try {
+        URL = "http://eulerity-hackathon.appspot.com/pets"
+        const response = await axios.get(URL)
+        const arr = response.data
+        arr.forEach((e) => {
+            createImage(e)
+        })
+    } catch (error) {
+        console.log(`${error}`)
+    }
+}
+
+getPets
