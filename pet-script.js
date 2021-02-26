501ddfb4ce64eb1481c9b0fdd605dc0d70f47a9b
Cashman1396 <Chrisashman96@gmail.com>
2021-02-26 13:11:59 -0500

    made createElement to retrieve images from server

--- pet-script.js
+++ pet-script.js
@@ -3,8 +3,20 @@ const imgGallery = new Array()
 
 
 //creates elements for downloading the images in the gallery
-const downloadBtn = doucment.createElement("div")
-downloadBtn.classList.add("downloadBtn")
+const download = doucment.createElement("div")
+download.classList.add("download")
 
 const downloadImg = document.createElement("button")
 downloadImg.innerHTML = "Download these images"
+downloadImg.classList.add("select-all-button")
+
+download.appendChild(downloadImg)
+
+
+//creation of images and retreiving from server
+const getImage = (data) => {
+    const rootDiv = document.createElement("div")
+    rootDiv.classList.add("img-container")
+
+    const img = doucment.createElement("img")
+}
